5277b124c9b7af9428e9abeae00e3f4d3042ad1f
unknown <pankaj.nimgade@gmail.com>
2018-03-03 17:51:35 -0600

    UI changes

--- app/src/main/java/pk/nimgade/calculator/view/MainActivity.kt
+++ app/src/main/java/pk/nimgade/calculator/view/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : SuperMain(), IMainActivityView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
